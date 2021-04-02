0974e88c3b30b39df2d9e2a07f0efb066b1d8fd8
Eldridge ribeiro <eldridgeriberio@gmail.com>
2021-04-02 22:48:19 +0100

    created vendor app

--- interiorshop/interiorshop/settings.py
+++ interiorshop/interiorshop/settings.py
@@ -11,17 +11,18 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
-from Secrets import SECRET_KEY
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+TEMPLATES_DIR = os.path.join(BASE_DIR, "templates")
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+SECRET_KEY = '#23t(gai$aq1^@0ondolx_()pd2p=spin5!q1u#7z93%as*$(u'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'core',
     'vendor',
 
@@ -58,7 +60,7 @@ ROOT_URLCONF = 'interiorshop.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATES_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -122,6 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-        BASE_DIR/'static'
-]
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
